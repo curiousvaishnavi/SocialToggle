17f9f3ba251b4a4a2eabffa8bcc08f0becfbc9d4
Vaishnavi Dethe <vaishnavidethe2810@gmail.com>
2026-01-09 23:12:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let add = document.querySelector("#add")
+let remove = document.querySelector("#remove")
+let h5 = document.querySelector("h5")
+let btn=document.querySelector("button")
+
+let flag = 0
+add.addEventListener("click",function(){
+    if(flag === 0){
+        btn.innerHTML="Remove Friend"
+        h5.innerHTML = "Friends"
+        h5.style.color = "green"
+        flag = 1
+    }else{
+        btn.innerHTML="Add Friend"
+        h5.innerHTML = "Strangers"
+        h5.style.color = "red"
+        flag = 0
+    }  
+});
